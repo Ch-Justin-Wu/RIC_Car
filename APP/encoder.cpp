a667884b8ae03d23875b6c95e69d65ace7389a6d
JustinWu <justin.wu@zllcrm.org>
2023-11-28 20:21:10 +0800

    edit README.md

--- APP/encoder.cpp
+++ APP/encoder.cpp
@@ -11,17 +11,16 @@ using namespace std;
  */
 void ENCODER_OUTPUT()
 {
-    
     switch (GPIO_Pin)
     {
     case ENCODER1_Pin:
 
         motors[0].Encoder_Count();
         break;
-   case ENCODER2_Pin:
+    case ENCODER2_Pin:
 
-       motors[1].Encoder_Count();
-       break;
+        motors[1].Encoder_Count();
+        break;
     case ENCODER3_Pin:
 
         motors[2].Encoder_Count();
